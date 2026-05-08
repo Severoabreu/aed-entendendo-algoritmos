584bce4ccfb293ae859b786d6387d0823ffb5d1f
Severo Abreu <Severoabreu9@gmail.com>
2026-05-07 23:29:13 -0300

    .

--- src/kth_to_last.py
+++ src/kth_to_last.py
@@ -2,4 +2,20 @@ from src.my_node import MyNode
 
 
 def kth_to_last(head: MyNode, k: int) -> int:
-    raise NotImplementedError
+    if not head or k <= 0:
+        return -1
+    
+    ponteiro_frente = head
+    ponteiro_atras = head
+
+
+    for _ in range (k):
+        if not ponteiro_frente:
+            return -1
+        ponteiro_frente = ponteiro_frente.next
+
+    while ponteiro_frente:
+        ponteiro_frente = ponteiro_frente.next
+        ponteiro_atras = ponteiro_atras.next
+
+    return ponteiro_atras.value
--- src/valid_parentheses.py
+++ src/valid_parentheses.py
@@ -1,5 +1,24 @@
-# from src.my_stack import MyStack
+from src.my_stack import MyStack
 
 
 def is_valid_parentheses(string: str) -> bool:
-    raise NotImplementedError
+    pilha = MyStack()
+
+    for caractere in string:
+        if caractere == '(' or caractere == '[' or caractere == '{':
+            pilha.push(caractere)
+
+        else:
+            if pilha.is_empty():
+                return False
+            
+            topo = pilha.pop()
+
+            if caractere == ')' and topo != '(':
+                return False
+            if caractere == ']' and topo != '[':
+                return False
+            if caractere == '}' and topo != '{':
+                return False
+            
+    return pilha.is_empty()
